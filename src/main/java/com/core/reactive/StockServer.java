bbc2fd783435aff062ffbf779704d6520d998948
tansudasli <tansudasli@gmail.com>
2021-03-15 21:11:25 +0300

    Enhanced emitting

--- src/main/java/com/core/reactive/StockServer.java
+++ src/main/java/com/core/reactive/StockServer.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class StockServer {
 
-    private static void process(ObservableEmitter<Stock> emitter, List<String> symbols) {
+    private static void emitThings(ObservableEmitter<Stock> emitter, List<String> symbols) {
 
         //emit the Stock
         //symbols.forEach(symbol -> emitter.onNext(fetch(symbol)));
@@ -20,28 +20,32 @@ public class StockServer {
         emitter.onComplete();  //send complete signal
     }
 
+    private static void processThings(Stock stock) {
+        //intercept and do some other staff, such as buy the stock
+        if (stock.getPrice() < 35) {
+            //unsubscribe, then buy, to do that we need to use anonymous new Observer<Stock>() { }
+            //and, override methods!!! or another way ??
+            System.out.println("BUY...");
+        }
+
+        //do something
+        System.out.println("got...: " + stock.toString());
+    }
+
     public static void main(String[] args) {
 
         var symbols = Arrays.asList("GOOG", "AMZN", "TESLA");
 
         System.out.println("server started...");
 
-        //create Observable, emit - do something, till it finishes, subscribe - to read data
-        Observable.<Stock>create(emitter -> process(emitter, symbols))
-                  .subscribe(stock -> {
-                                //intercept and do some other staff, such as buy the stock
-                                if (stock.getPrice() < 35) {
-                                    //unsubscribe, then buy, to do that we need to use anonymous new Observer<Stock>() { }
-                                    //and, override methods!!! or another way ??
-                                    System.out.println("BUY...");
-                                }
-
-                                //do something
-                                System.out.println("got...: " + stock.toString());
-                             }, //onNext
-                             err -> System.out.println("ERROR..."),  //onError channel
-                             () -> System.out.println("DONE..."))             //onComplete channel
+        //create Observable<Stock), emit - do something,
+        //then, subscribe - to read data
+        Observable.<Stock>create(emitter -> emitThings(emitter, symbols))
+                  .subscribe(StockServer::processThings,                    //onNext
+                             err -> System.out.println("ERROR..." + err),  //onError channel
+                             () -> System.out.println("DONE..."))          //onComplete channel
                   .dispose();  //unsubscribe
 
     }
+
 }
