f7c06c5f94db2c8dacdb0841ff585ba2555f90ea
tansudasli <tansudasli@gmail.com>
2021-03-19 19:45:01 +0300

    added details of class level types T,U..

---
+++ src/main/java/com/core/generics/Product.java
@@ -0,0 +1,62 @@
+package com.core.generics;
+
+public class Product<T, U> {
+
+    private T id;    //at class level we added T, so class is generic type
+    private U name;  //so we did not used T, means name can be different type
+    private T description;
+
+    public Product(T id, U name, T description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    public U getName() {
+        return name;
+    }
+
+    public void setName(U name) {
+        this.name = name;
+    }
+
+    public T getDescription() {
+        return description;
+    }
+
+    public void setDescription(T description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name=" + name +
+                ", description=" + description +
+                '}';
+    }
+
+    //lets test
+    public static void main(String[] args) {
+
+        /*
+         * T int, U string...
+         * so, id, desc must be int. Or we need to add more generic types at class level like <T,U,W>!
+         */
+        Product<Integer, String> product = new Product<Integer, String>(1, "Tansu", 1000);
+
+        System.out.println(product.toString());
+    }
+}
+
+
