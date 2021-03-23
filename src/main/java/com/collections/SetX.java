70430937d43889c8c1a27929d7c7cd8cf6a1bdad
tansudasli <tansudasli@gmail.com>
2021-03-23 19:13:32 +0300

    Collections - set (added insert-order and sorted-order capabilities)

--- src/main/java/com/collections/SetX.java
+++ src/main/java/com/collections/SetX.java
@@ -1,26 +1,43 @@
 package com.collections;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetX {
     /*
      * Set
      *
      * - unique items, no index-based access
-     *
-     *
-     *
-     *
-     *
+     *                 insertion-order       sorted-order
+     * set                      x               x
+     * HashSet                  x               x
+     * LinkedHashSet           yes              x
+     * TreeSet                  x              yes
      */
 
     public static void main(String[] args) {
         Set<String> set = Set.of("Ali", "Veli", "Malim", "Halim");
         Set<String> setHashed = new HashSet<>(set);
+        Set<String> setLinkedHashed = new LinkedHashSet<>(set);
+        Set<String> setTree = new TreeSet<>(set);
 
-        //Set does not care about initialized order ! Use hashset if it is critical.
+        //Set does not care about initialized order or search order !
         System.out.println(set);
         System.out.println(setHashed);
+        System.out.println(setLinkedHashed);
+        System.out.println(setTree);
+
+        //insertion-order and sorted-order diffs.
+        Set<Integer> hashed = new HashSet<>(); hashed.add(1); hashed.add(100); hashed.add(10); hashed.add(1000);
+        Set<Integer> linkedHashed = new LinkedHashSet<>(); linkedHashed.add(1); linkedHashed.add(100); linkedHashed.add(10); linkedHashed.add(1000);
+        Set<Integer> tree = new TreeSet<>(); tree.add(1); tree.add(100); tree.add(10); tree.add(1000);
+
+        System.out.println(hashed);
+        System.out.println(linkedHashed);
+        System.out.println(tree);
+
+
     }
 }
