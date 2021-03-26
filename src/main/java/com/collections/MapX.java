50f5eff2f8b3db9deb09185b167c06f535d27560
tansudasli <tansudasli@gmail.com>
2021-03-26 15:59:41 +0300

    Collections - map (navigable int. functs.)

--- src/main/java/com/collections/MapX.java
+++ src/main/java/com/collections/MapX.java
@@ -1,7 +1,8 @@
 package com.collections;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static java.util.Arrays.stream;
 
 public class MapX {
     /*
@@ -9,16 +10,16 @@ public class MapX {
      *
      * - HashMap: unsorted, unordered, allows NULL value.
      * - HashTable: synced, thread-safe, unsorted, unordered
-     * - LinkedHashMap: order preserved, (so insertion, deletion a bit slower than HashMap)
-     * - TreeMap: sorted
+     * - LinkedHashMap: insert-order preserved, (so insertion, deletion a bit slower than HashMap)
+     * - TreeMap: sorted (default natural order)
      *
      *
      */
 
     public static void main(String[] args) {
 
-        Map<String, Integer> map = Map.of("01234h", 2, "e5YH778", 4,
-                                          "ha782",1, "83h8ihs", 3);
+        Map<String, Integer> map = Map.of("a01234h", 2, "e5YH778", 4,
+                                          "ha782",1, "b83h8ihs", 3);
 
         Map<String, Integer> hashMap = new HashMap<>(map);
 
@@ -39,6 +40,39 @@ public class MapX {
         hashMap.computeIfPresent("01234h", (k, v) -> v * v * v);
         System.out.println(hashMap);
 
+        //insert-order preserved
+        LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>(map);
+        System.out.println(linkedHashMap);
+
+        //sorted-order
+        TreeMap<String, Integer> treeMap = new TreeMap<>(map);
+        TreeMap<String, Integer> treeMap2 = new TreeMap<>(Comparator.reverseOrder());  //sorts by key !
+        TreeMap<String, Integer> treeMap3 = new TreeMap<>(Comparator.comparing(String::length).reversed());
+
+        treeMap2.putAll(map);
+        treeMap3.putAll(map);
+
+        System.out.println("treemap: " + treeMap);
+        System.out.println(treeMap2);
+        System.out.println(treeMap3);
+
+        //some methods specific to maps (comes w/ navigable interface)
+        System.out.println(treeMap.higherKey("e5YH778"));
+        System.out.println(treeMap.ceilingKey("e5YH778"));
+
+        String line = "This is not me, Me is not me";
+
+        Map<Character, Integer> occurrences = new HashMap<>();
+        for (Character ch: line.toLowerCase(Locale.ROOT).toCharArray()) {
+
+            if (occurrences.containsKey(ch)) {
+                occurrences.put(ch, occurrences.get(ch) + 1);
+            } else {
+                occurrences.put(ch, 1);
+            }
+        }
+
+        System.out.println(occurrences);
 
     }
 
