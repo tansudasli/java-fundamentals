df93170829913661353bbd458cb1c79a801bf17b
tansudasli <tansudasli@gmail.com>
2021-03-27 18:46:50 +0300

    Callable, Future - returning multiple values from threads

---
+++ src/main/java/com/multithread/ThreadReturnMultipleValues.java
@@ -0,0 +1,50 @@
+package com.multithread;
+
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Function;
+
+
+
+public class ThreadReturnMultipleValues {
+
+    private static String handleTask(String name) throws InterruptedException {
+        Thread.sleep(1000);
+
+        return "Hello " + name + " " + Thread.currentThread();
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        //executor
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+
+        List<Task> tasks = List.of(new Task("Tansu"), new Task("Zabid"), new Task("Abidin"));
+
+        //trigger
+//        List<Future<String>> results = exec.invokeAll(tasks);   //List<Future<String>>
+//        for (Future<String> r : results)
+//            System.out.println(r.get());
+
+        exec.invokeAll(tasks).forEach(c-> {
+            try {
+                System.out.println(c.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+//
+//        exec.invokeAll(tasks).stream().map(s -> {
+//            try {
+//                return s.get();
+//            } catch (InterruptedException | ExecutionException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }).forEach(System.out::println);
+
+        //shutdown
+        exec.shutdown();
+
+    }
+}
