a482f1805f0b63a3f170ef6e003b68bc698a9442
tansudasli <tansudasli@gmail.com>
2021-03-27 16:16:51 +0300

    Callable - returning values from threads

---
+++ src/main/java/com/multithread/ThreadReturnValues.java
@@ -0,0 +1,50 @@
+package com.multithread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Task implements Callable<String> {
+    String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+
+        return "Hello " + name + " " + Thread.currentThread();
+    }
+}
+public class ThreadReturnValues {
+    /*
+     * Returning Values from Threads
+     *
+     *
+     */
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+        String result = exec.submit(new Task("Tansu"))
+                            .get();
+
+        System.out.println(result);
+
+        //a Task that implemented Callable interface
+        System.out.println(
+                exec.submit(() -> {
+
+                    Thread.sleep(1000);
+
+                    return "Hello " + Thread.currentThread();
+                })                //Future<String
+                .get()            //String
+        );
+
+
+    }
+}
