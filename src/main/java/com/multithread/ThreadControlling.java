0f59fd19a37e77b597331cce624cfc246c749be4
tansudasli <tansudasli@gmail.com>
2021-03-27 15:45:43 +0300

    ExecutorService - multi-thread approach

--- src/main/java/com/multithread/ThreadControlling.java
+++ src/main/java/com/multithread/ThreadControlling.java
@@ -6,16 +6,29 @@ import java.util.concurrent.Executors;
 public class ThreadControlling {
     /*
      * Thread Controlling
-     *
+     *  - Executors.newSingleThreadExecutor() : 1 thread in pool
+     *  - Executors.newFixedThreadPool(n)    : n threads in pool
      *
      *
      */
 
     public static void main(String[] args) {
-        ExecutorService exec = Executors.newSingleThreadExecutor();  //execs single thread at a time
+//        ExecutorService exec = Executors.newSingleThreadExecutor();  //execs single thread at a time
+        ExecutorService exec = Executors.newFixedThreadPool(3);
 
         exec.execute(new Task1());
-        exec.execute(new Thread(new Task2()));  // exec.execute(new Task2());
+        exec.execute(new Task2());  //exec.execute(new Thread(new Task2()));  no need a separate thread allocation first
+
+        exec.execute(() -> {
+            System.out.println("Task-3 started");
+
+            for (int i = 301 ; i < 351; i++)
+                System.out.println(i + " " + Thread.currentThread());
+
+            System.out.println("Task-3 done");
+        });
+
+        exec.shutdown();
 
     }
 }
