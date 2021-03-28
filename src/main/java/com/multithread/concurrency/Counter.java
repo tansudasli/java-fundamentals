87f514df234b0d890a5915c89e2cd87c6c93b5f4
tansudasli <tansudasli@gmail.com>
2021-03-28 18:28:02 +0300

    Added synchronized keyword

--- src/main/java/com/multithread/concurrency/Counter.java
+++ src/main/java/com/multithread/concurrency/Counter.java
@@ -1,14 +1,26 @@
 package com.multithread.concurrency;
 
-//non atomic class
+//non-atomic class
 public class Counter {
-    int i = 0;
+    private int i = 0;
+    private int j = 0;
 
     /* below is not atomic as 3 operations happen below
      * what will happen 3 threads trying to increment? it is not thread-safe
+     *
+     * So, we can
+     * 1- synchronized: but only 1 thread can use this class. even there are different methods!! not performant.
+     * 2- lock
      */
-    public void increment() {
+    synchronized public void incrementI() {
         i++;
+        //get i
+        //increment i
+        //set i
+    }
+
+    synchronized public void incrementJ() {
+        j++;
         //get i
         //increment i
         //set i
@@ -16,5 +28,8 @@ public class Counter {
 
     public int getI() {
         return i;
+    }
+    public int getJ() {
+        return j;
     }
 }
