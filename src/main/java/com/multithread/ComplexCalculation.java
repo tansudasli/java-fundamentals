624d879c63798757604721bfe11c9a338de35daf
tansudasli <tansudasli@gmail.com>
2021-04-07 16:21:55 +0300

    Multithreaded but blocking call

--- src/main/java/com/multithread/ComplexCalculation.java
+++ src/main/java/com/multithread/ComplexCalculation.java
@@ -1,8 +1,8 @@
 package com.multithread;
 
 import java.math.BigInteger;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class ComplexCalculation {
 
@@ -10,13 +10,24 @@ public class ComplexCalculation {
 
         BigInteger result = BigInteger.ONE;
 
+        System.out.println(Thread.currentThread().getName() + "..started  ");
+
         if (power == 0)
             return result;
 
         for (int i = 0; i < power; i++) {
-            //todo: handle interruption
+            System.out.println(Thread.currentThread().getName() + "  " + i);
+            //handle interruption
+            if (Thread.currentThread().isInterrupted()) {
+
+                result = BigInteger.ZERO;
+                break;
+            }
+
             result = result.multiply(BigInteger.valueOf(base));
         }
+
+        System.out.println(Thread.currentThread().getName() + "  " + result);
 
         return result;
     }
@@ -40,14 +51,39 @@ public class ComplexCalculation {
          *
          */
 
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        try {
+            /*
+             * if we use get() like below, then it blocks!!
+             * BigInteger r1 = executorService.submit(() -> calculatePower(200,1000)).get();
+             *
+             * when we split lines, it becomes non-blocking
+             * Callable<BigInteger> t1 = () -> calculatePower(2,1000);
+             * Future<BigInteger> r1 = executorService.submit(t1);
+             */
 
 
-        BigInteger sum = sumOfPowers(2, 1, 3, 1);
 
-        assert (sumOfPowers(2, 1, 3, 1).equals(BigInteger.valueOf(5)));
-        assert (sumOfPowers(2, 2, 3, 2).equals(BigInteger.valueOf(13)));
+            //below is blocking. thread2 does not start till thread1 ends. due to get() call
+           BigInteger r1 = executorService.submit(() -> calculatePower(200,1000)).get();
+           BigInteger r2 = executorService.submit(() -> calculatePower(3,1000)).get();
 
-        System.out.println(sum);
+            System.out.println(r1.add(r2));
 
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+        //serial version
+//        BigInteger sum = sumOfPowers(2, 1, 3, 1);
+//
+//        assert (sumOfPowers(2, 1, 3, 1).equals(BigInteger.valueOf(5)));
+//        assert (sumOfPowers(2, 2, 3, 2).equals(BigInteger.valueOf(13)));
+
+//        System.out.println(sum);
+
+        executorService.shutdown();
     }
 }
