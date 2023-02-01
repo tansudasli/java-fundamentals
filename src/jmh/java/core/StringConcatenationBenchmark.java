1643d1b1441b12a6905ab757a43c6cc39386c7f4
tansudasli <tansudasli@gmail.com>
2023-02-01 15:12:52 +0300

    Benchmark enhancements

--- src/jmh/java/core/StringConcatenationBenchmark.java
+++ src/jmh/java/core/StringConcatenationBenchmark.java
@@ -13,7 +13,7 @@ public class StringConcatenationBenchmark {
     /* concatenation. + is default operator
 
        Small manipulations:
-         - Use + over String.concat().
+         - Use + over String.concat(). String.join() etc.. for clarity..
        in a Loop or more ops.,
          - use StringBuilder or StringBuffer (threadsafe)
 
@@ -26,19 +26,17 @@ public class StringConcatenationBenchmark {
     //    + operator is x1.2 slower than builder
 //    So. 4 ways to concatenate
 //
-//    Benchmark                                             Mode  Cnt     Score     Error   Units
-//    StringConcatenationBenchmark.concatWithBuffer        thrpt    4  1354.827 ± 848.650  ops/us
-//    StringConcatenationBenchmark.concatWithBuilder       thrpt    4  1233.561 ± 208.028  ops/us
-//    StringConcatenationBenchmark.concatWithConcat        thrpt    4    12.214 ±  28.881  ops/us
-//    StringConcatenationBenchmark.concatWithJoin          thrpt    4     5.028 ±   4.502  ops/us
-//    StringConcatenationBenchmark.concatWithJoinStream    thrpt    4     4.181 ±   7.177  ops/us
-//    StringConcatenationBenchmark.concatWithPlusOperator  thrpt    4   228.719 ± 355.720  ops/us
-//    StringConcatenationBenchmark.concatWithBuffer         avgt    4     0.001 ±   0.002   us/op
-//    StringConcatenationBenchmark.concatWithBuilder        avgt    4     0.001 ±   0.004   us/op
-//    StringConcatenationBenchmark.concatWithConcat         avgt    4     0.094 ±   0.073   us/op
-//    StringConcatenationBenchmark.concatWithJoin           avgt    4     0.184 ±   0.058   us/op
-//    StringConcatenationBenchmark.concatWithJoinStream     avgt    4     0.213 ±   0.095   us/op
-//    StringConcatenationBenchmark.concatWithPlusOperator   avgt    4     0.004 ±   0.004   us/op
+//Benchmark                                             Mode  Cnt     Score     Error   Units
+//StringConcatenationBenchmark.concatWithBuffer        thrpt    6  1624.685 ± 168.801  ops/us
+//StringConcatenationBenchmark.concatWithBuilder       thrpt    6  1397.534 ± 157.024  ops/us
+//StringConcatenationBenchmark.concatWithConcat        thrpt    6    20.267 ±   3.779  ops/us
+//StringConcatenationBenchmark.concatWithJoin          thrpt    6     6.530 ±   0.539  ops/us
+//StringConcatenationBenchmark.concatWithPlusOperator  thrpt    6  1311.588 ±  72.581  ops/us
+//StringConcatenationBenchmark.concatWithBuffer         avgt    6     0.001 ±   0.001   us/op
+//StringConcatenationBenchmark.concatWithBuilder        avgt    6     0.001 ±   0.001   us/op
+//StringConcatenationBenchmark.concatWithConcat         avgt    6     0.046 ±   0.003   us/op
+//StringConcatenationBenchmark.concatWithJoin           avgt    6     0.160 ±   0.023   us/op
+//StringConcatenationBenchmark.concatWithPlusOperator   avgt    6     0.001 ±   0.001   us/op
 
     @Benchmark
     @BenchmarkMode({Mode.AverageTime, Mode.Throughput})
@@ -90,15 +88,6 @@ public class StringConcatenationBenchmark {
         bh.consume(StringTest.concatWithBuffer);
     }
 
-    @Benchmark
-    @BenchmarkMode({Mode.AverageTime, Mode.Throughput})
-    @Fork(2)
-    @Warmup(iterations = 1, time = 1)
-    @Measurement(iterations = 1, time = 1)
-    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public static void concatWithJoinStream(Blackhole bh) {
-        bh.consume(StringTest.concatWithJoinStream.get());
-    }
 
     // java -cp build/libs/java-fundamentals-1.0-SNAPSHOT-jmh.jar core.StringConcatenationBenchmark
     public static void main(String[] args) throws Exception {
@@ -106,7 +95,6 @@ public class StringConcatenationBenchmark {
         var opt = new OptionsBuilder()
                 .include(core.StringConcatenationBenchmark.class.getName())
                 .jvmArgs("-Xms1g", "-Xmx1g", "-XX:+UseG1GC")
-                .warmupForks(1)
                 .warmupIterations(1)
                 .measurementIterations(2)
                 .forks(3)
